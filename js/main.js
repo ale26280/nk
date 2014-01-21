e87f803bf23c3735c1f47d535256b1e2f877ed03
ale26280 <alejandrocduarte@gmail.com>
2014-01-20 23:07:38 -0300

    subiend modo2

--- js/main.js
+++ js/main.js
@@ -161,10 +161,11 @@ item = localStorage.getItem(localStorage.key(0)).split('|')
 */
 
 		for (var i = 0; i < 1; i++){
-    alert(localStorage.getItem(localStorage.key(i)));
+		inicia++
+    //alert(localStorage.getItem(localStorage.key(i)));
     todo = localStorage.getItem(localStorage.key(i));
     p = todo.split('|');
-    alert(p[0]);
+    //alert(p[0]);
     cargaDesdeLocal(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11]);
     localStorage.removeItem(localStorage.key(i));
 			}	
