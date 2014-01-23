533dc8a99170b2696915e19942dd9054f6b59c44
ale26280 <alejandrocduarte@gmail.com>
2014-01-23 20:04:41 -0300

    subiend

--- js/main.js
+++ js/main.js
@@ -554,7 +554,7 @@ if(p2[14]) {
                         	}else{
 */
                             cargaDesdeLocalActualizado(p2[0], p2[1], p2[2], p2[3], p2[4], p2[5], p2[6], p2[7], p2[8], p2[9], p2[10], p2[11], p2[12]);
-                            localStorage.removeItem('' + p2[12] + '';
+                            localStorage.removeItem('' + p2[12] + '');
                             //localStorage.setItem('' + p2[12] + '', p2[0] + '|' + p2[1] + '|' + p2[2] + '|' + p2[3] + '|' + p2[4] + '|' + p2[5] + '|' + p2[6] + '|' +  p2[7] + '|' + p2[8] + '|' + p2[9] + '|' + p2[10] + '|' + p2[11] + '|' + p2[12] + '|' + p2[13] + '|' + 'comprobado');
                             return false;
 							//}
