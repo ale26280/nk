eab1a81bd3c8bf7bb3ed0de6c8b20fb3520f599c
ale26280 <alejandrocduarte@gmail.com>
2014-01-23 13:15:21 -0300

    subiend

--- js/main.js
+++ js/main.js
@@ -240,7 +240,8 @@ alert(locales)
                     	//alert('p01 '+p[0])
                         cargaDesdeLocal(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11], p[12]);
                         agregaLSActualizado(p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10], p[11], p[12])
-                        localStorage.removeItem(localStorage.key(i));
+                        alert(localStorage.getItem('' + p[12] + ''))
+                        localStorage.removeItem('' + p[12] + '');
                         return false;
                     }
                     
