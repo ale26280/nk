3fb3948a2f1e1d9b6c35321c1d87e8f735d4def5
ale26280 <alejandrocduarte@gmail.com>
2014-01-23 16:24:33 -0300

    subiend

--- js/main.js
+++ js/main.js
@@ -543,7 +543,7 @@ function compruebaDbLocalActualizados(v) {
                         //alert(p[0]);
                         if (p2[13]) {
                         	alert(todo);
-                        	comprobados++;
+                        	
                         	//if(p2[14]) {
                         	//}else{
                             cargaDesdeLocalActualizado(p2[0], p2[1], p2[2], p2[3], p2[4], p2[5], p2[6], p2[7], p2[8], p2[9], p2[10], p2[11], p2[12]);
@@ -588,7 +588,9 @@ function cargaDesdeLocalActualizado(nombre, apellido, dia, mes, ano, telefono, d
         origen: origenD,
         fecha: fecha
     }, function (data) {
-        alert(data);
+        //if(data==1){
+	        comprobados++;
+       
         if (imgD != 'no') {
             uploadPhotoLocalActualizado(imgD);
             //localStorage.removeItem(key);
