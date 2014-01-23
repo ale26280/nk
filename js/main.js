4b04e88093af957e379430f6c83b0fd63cd50fbe
ale26280 <alejandrocduarte@gmail.com>
2014-01-23 20:56:40 -0300

    subiend

--- js/main.js
+++ js/main.js
@@ -535,18 +535,18 @@ function compruebaDbLocalActualizados(v) {
 			
 			//inicia = 0;
             //alert(localStorage.length)
-			alert('carga aca')
+			alert('carga ')
 
                 for (var i = 0; i < localStorage.length; i++) {
                     //inicia++
                     //alert(localStorage.getItem(localStorage.key(i)));
                     todo2 = localStorage.getItem(localStorage.key(i));
-                    var n2 = todo.indexOf("|");
+                    var n2 = todo2.indexOf("|");
                     if (n2 == '-1') {} else {
                         p2 = todo2.split('|');
                         //alert(p[0]);
                         //if (p2[13]) {
-                        	alert(todo);
+                        	alert(todo2);
                         	
                         	/*
 if(p2[14]) {
@@ -554,7 +554,7 @@ if(p2[14]) {
                         	}else{
 */
                             cargaDesdeLocalActualizado(p2[0], p2[1], p2[2], p2[3], p2[4], p2[5], p2[6], p2[7], p2[8], p2[9], p2[10], p2[11], p2[12]);
-                            //localStorage.removeItem('' + p2[12] + '');
+                            localStorage.removeItem('' + p2[12] + '');
                             //localStorage.setItem('' + p2[12] + '', p2[0] + '|' + p2[1] + '|' + p2[2] + '|' + p2[3] + '|' + p2[4] + '|' + p2[5] + '|' + p2[6] + '|' +  p2[7] + '|' + p2[8] + '|' + p2[9] + '|' + p2[10] + '|' + p2[11] + '|' + p2[12] + '|' + p2[13] + '|' + 'comprobado');
                             return false;
 							//}
